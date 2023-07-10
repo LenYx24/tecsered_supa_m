648ae711ced7f9ce3ab409cd9aabf788b7ae2e1c
LenYx24 <fokilenard@gmail.com>
2023-07-10 19:53:41 +0200

    fix auth/callback react component

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,16 +1,16 @@
-import type { NextApiHandler } from "next";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import type { Database } from "lib/database.types";
+// import type { NextApiHandler } from "next";
+// import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+// import type { Database } from "lib/database.types";
 
-const handler: NextApiHandler = async (req, res) => {
-  const { code } = req.query;
+// const handler: NextApiHandler = async (req, res) => {
+//   const { code } = req.query;
 
-  if (code) {
-    const supabase = createPagesServerClient<Database>({ req, res });
-    await supabase.auth.exchangeCodeForSession(String(code));
-  }
+//   if (code) {
+//     const supabase = createPagesServerClient<Database>({ req, res });
+//     await supabase.auth.exchangeCodeForSession(String(code));
+//   }
 
-  res.redirect("/");
-};
+//   res.redirect("/");
+// };
 
-export default handler;
+// export default handler;
